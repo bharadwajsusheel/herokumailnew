035cef1b6128a38a5e50535a662f6d2980c8e005
susheelbharadwaj <Bsusheel@techzert.local>
2021-01-02 20:56:43 +0530

    initial commit

---
+++ dbConnect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: __dirname + '/.env' });
+
+function dbConnect(DB_URL) {
+    const connectionOptions = {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        autoIndex: false
+    };
+    mongoose.connect(DB_URL, connectionOptions)
+        .then(() => console.log('Connected to DB...'))
+        .catch(err => console.log(`DB Connection Error: ${err}`));
+}
+
+module.exports = dbConnect;
